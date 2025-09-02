698c05ef28d72220fcb7f3fd710590a7e515ad94
Ahmed Galal <ahmed.galal@propertiano.com>
2025-09-02 23:02:41 +0300

    fix

--- src/modules/ParallexFour/parallex.jsx
+++ src/modules/ParallexFour/parallex.jsx
@@ -168,11 +168,15 @@ const ParallaxFourSection = () => {
 
                 {/* Safety Features Swiper Section */}
                 <div className='w-screen p-10 h-[90vh] bg-[#05141f]'>
+                    <div className="relative h-full w-full">
                     <Swiper
                         modules={[Navigation]}
                         spaceBetween={0}
                         slidesPerView={1}
-                        navigation={true}
+                            navigation={{
+                                nextEl: '.swiper-button-next-custom',
+                                prevEl: '.swiper-button-prev-custom',
+                            }}
                         autoplay={false}
                         
                         loop={false}
@@ -257,14 +261,18 @@ const ParallaxFourSection = () => {
                     </Swiper>
 
                     {/* Custom Navigation Buttons */}
-                    <button className="absolute top-1/2 right-0 z-[1000] text-white bg-green w-10 h-10 left-4">
-                        {' >'}
+                        <button className="swiper-button-prev-custom absolute -left-4  cursor-pointer top-1/2 transform -translate-y-1/2 z-10 w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-lg hover:bg-gray-100 transition-colors duration-200">
+                            <svg className="w-6 h-6 text-[#05141f]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                            </svg>
                     </button>
-                    <button className="absolute z-[1000] right-4">
-                        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+
+                        <button className="swiper-button-next-custom absolute -right-4 cursor-pointer top-1/2 transform -translate-y-1/2 z-10 w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-lg hover:bg-gray-100 transition-colors duration-200">
+                            <svg className="w-6 h-6 text-[#05141f]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
                         </svg>
                     </button>
+                    </div>
 
                     
                 </div>
